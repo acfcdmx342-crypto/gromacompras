d2bda6a6dc1fe7e6f2a782b817b2d0686d0b272a
Luis Garcia <acf.cdmx.34.2@gmail.com>
2025-12-15 17:21:20 -0600

    Add files via upload

---
+++ README.md
@@ -0,0 +1,10 @@
+
+# ACF Compras – Firebase
+
+Sistema completo con:
+- Login
+- Base de personal
+- Tecnología
+- Seguimiento
+
+Usa Firebase Realtime Database y funciona en GitHub Pages.
--- firebase.js
+++ firebase.js
@@ -1,15 +1,17 @@
 
-// Firebase Config
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+import { getDatabase } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-database.js";
+import { getStorage } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-storage.js";
+
 const firebaseConfig = {
   apiKey: "AIzaSyDakM1BpK2EF8QrMcurDVodrG8LhVbtydo",
   authDomain: "mydbu-fbc33.firebaseapp.com",
   projectId: "mydbu-fbc33",
   storageBucket: "mydbu-fbc33.firebasestorage.app",
   messagingSenderId: "3794457198",
-  appId: "1:3794457198:web:b591c3bdfbb06f1cf54c7f",
-  measurementId: "G-NXX5XDXYWB",
-  databaseURL: "https://mydbu-fbc33-default-rtdb.firebaseio.com"
+  appId: "1:3794457198:web:b591c3bdfbb06f1cf54c7f"
 };
 
-const app = firebase.initializeApp(firebaseConfig);
-const db = firebase.database();
+export const app = initializeApp(firebaseConfig);
+export const db = getDatabase(app);
+export const storage = getStorage(app);
