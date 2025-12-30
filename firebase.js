ff0a852a242c735c4f80095a50cf453900366dca
Luis Garcia <acf.cdmx.34.2@gmail.com>
2025-12-30 11:36:53 -0600

    Set up Firebase app and database connection
    Initialize Firebase app and database with configuration.

--- firebase.js
+++ firebase.js
@@ -1,3 +1,23 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-app.js";
+import { getDatabase, ref, get, child } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-database.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDakM1BpK2EF8QrMcurDVodrG8LhVbtydo",
+  authDomain: "mydbu-fbc33.firebaseapp.com",
+  databaseURL: "https://mydbu-fbc33-default-rtdb.firebaseio.com/",
+  projectId: "mydbu-fbc33",
+  storageBucket: "mydbu-fbc33.firebasestorage.app",
+  messagingSenderId: "3794457198",
+  appId: "1:3794457198:web:b591c3bdfbb06f1cf54c7f"
+};
+
+const app = initializeApp(firebaseConfig);
+const database = getDatabase(app);
+
+export { database, ref, get, child };
+
+
+
 // firebase.js
 
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-app.js";
