9673b740042d7b1cd24b1aea19cda89f77bea78f
Luis Garcia <acf.cdmx.34.2@gmail.com>
2025-12-30 11:28:00 -0600

    Refactor Firebase imports and configuration

--- firebase.js
+++ firebase.js
@@ -1,17 +1,38 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-app.js";
-import { getDatabase, ref, get, child } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-database.js";
+// firebase.js
 
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-app.js";
+import {
+  getDatabase,
+  ref,
+  push,
+  set,
+  get,
+  onValue,
+  remove
+} from "https://www.gstatic.com/firebasejs/9.23.0/firebase-database.js";
+
+/* ================= CONFIGURACIÓN ================= */
 const firebaseConfig = {
   apiKey: "AIzaSyDakM1BpK2EF8QrMcurDVodrG8LhVbtydo",
   authDomain: "mydbu-fbc33.firebaseapp.com",
-  databaseURL: "https://mydbu-fbc33-default-rtdb.firebaseio.com/",
+  databaseURL: "https://mydbu-fbc33-default-rtdb.firebaseio.com",
   projectId: "mydbu-fbc33",
-  storageBucket: "mydbu-fbc33.firebasestorage.app",
+  storageBucket: "mydbu-fbc33.appspot.com",
   messagingSenderId: "3794457198",
   appId: "1:3794457198:web:b591c3bdfbb06f1cf54c7f"
 };
 
+/* ================= INICIALIZAR ================= */
 const app = initializeApp(firebaseConfig);
 const database = getDatabase(app);
 
-export { database, ref, get, child };
+/* ================= EXPORTAR ================= */
+export {
+  database,
+  ref,
+  push,
+  set,
+  get,
+  onValue,
+  remove
+};
