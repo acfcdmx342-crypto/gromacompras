33a85b33044ff34394901b44992df39390cb6b3b
Luis Garcia <acf.cdmx.34.2@gmail.com>
2025-12-30 14:49:08 -0600

    Refactor firebase.js to include update function

--- firebase.js
+++ firebase.js
@@ -1,5 +1,3 @@
-// firebase.js
-
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-app.js";
 import {
   getDatabase,
@@ -7,34 +5,31 @@ import {
   push,
   set,
   get,
-  child,
   onValue,
-  remove
+  remove,
+  update
 } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-database.js";
 
-/* ================= CONFIGURACIÓN ================= */
 const firebaseConfig = {
   apiKey: "AIzaSyDakM1BpK2EF8QrMcurDVodrG8LhVbtydo",
   authDomain: "mydbu-fbc33.firebaseapp.com",
-  databaseURL: "https://mydbu-fbc33-default-rtdb.firebaseio.com/",
+  databaseURL: "https://mydbu-fbc33-default-rtdb.firebaseio.com",
   projectId: "mydbu-fbc33",
   storageBucket: "mydbu-fbc33.appspot.com",
   messagingSenderId: "3794457198",
   appId: "1:3794457198:web:b591c3bdfbb06f1cf54c7f"
 };
 
-/* ================= INICIALIZAR ================= */
 const app = initializeApp(firebaseConfig);
 const database = getDatabase(app);
 
-/* ================= EXPORTAR ================= */
 export {
   database,
   ref,
   push,
   set,
   get,
-  child,
   onValue,
-  remove
+  remove,
+  update
 };
