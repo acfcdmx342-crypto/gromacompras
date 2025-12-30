80ffa89207eedfb04dffd51bc3879c2881fa9560
Luis Garcia <acf.cdmx.34.2@gmail.com>
2025-12-29 18:21:58 -0600

    Upgrade Firebase to version 9.23.0 and add data reading example
    Updated Firebase imports to version 9.23.0 and modified database URL. Added example code for reading data from the database.

--- firebase.js
+++ firebase.js
@@ -1,18 +1,24 @@
 
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
-import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
-import { getDatabase } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-database.js";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-app.js";
+import { getDatabase, ref, onValue, push } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-database.js";
 
 const firebaseConfig = {
   apiKey: "AIzaSyDakM1BpK2EF8QrMcurDVodrG8LhVbtydo",
   authDomain: "mydbu-fbc33.firebaseapp.com",
-  databaseURL: "https://mydbu-fbc33.firebaseio.com"
+  databaseURL: "https://mydbu-fbc33-default-rtdb.firebaseio.com/"
   projectId: "mydbu-fbc33",
   storageBucket: "mydbu-fbc33.firebasestorage.app",
   messagingSenderId: "3794457198",
   appId: "1:3794457198:web:b591c3bdfbb06f1cf54c7f"
 };
 
-export const app = initializeApp(firebaseConfig);
-export const db = getDatabase(app);
-export const storage = getStorage(app);
+
+const app = initializeApp(firebaseConfig);
+const database = getDatabase(app);
+
+// EJEMPLO lectura
+const dataRef = ref(database, "datos");
+onValue(dataRef, (snapshot) => {
+  const data = snapshot.val();
+  console.log(data);
+});
